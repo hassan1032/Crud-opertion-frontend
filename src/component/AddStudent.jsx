4c65c1e49ad380428666fda9605fe21cadf5b576
hassan1032 <khanhassan1032@gmail.com>
2024-04-29 18:58:04 +0530

    add css code

--- src/component/AddStudent.jsx
+++ src/component/AddStudent.jsx
@@ -8,20 +8,21 @@ const AddStudent = () => {
     <form className='student-form'>
         <h2 className='a-2'>Add Student</h2>
         <div className="form-group">
-            <label htmlFor="roll">Roll No</label>
-            <input type="text" id="roll"name="roll"/>
+            <label htmlFor="roll" className='label'>Roll No</label>
+            <input type="text" id="roll"name="roll" placeholder='Enter Your Roll Num'/>
         </div>
         <div className="form-group">
-            <label htmlFor="username">User Name</label>
-            <input type="text" id="username"name="username"/>
+            <label htmlFor="username" className='label'>User Name</label>
+            <input type="text" id="username"name="username" placeholder='Enter Your User Name '
+            />
         </div>
         <div className="form-group">
-            <label htmlFor="grade">Grade</label>
-            <input type="text" id="grade"name="grade"/>
+            <label htmlFor="grade" className='label'>Grade</label>
+            <input type="text" id="grade"name="grade" placeholder='Enter Your Grade'/>
         </div>
         <div className="form-group">
-            <label htmlFor="password">password</label>
-            <input type="password" id="password"name="password"/>
+            <label htmlFor="password" className='label'>password</label>
+            <input type="password" id="password"name="password" placeholder='Enter your Password'/>
         </div>
         <button type='submit' className='butt-'>Register</button>
     </form>
